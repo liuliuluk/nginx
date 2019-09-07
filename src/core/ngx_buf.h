2033f642f7e9bc742afe65c1b321025b20b5c376
JessyLuk <lukxuan@foxmail.com>
2019-09-07 08:48:25 +0800

    update
    add comments

--- src/core/ngx_buf.h
+++ src/core/ngx_buf.h
@@ -18,47 +18,75 @@ typedef void *            ngx_buf_tag_t;
 typedef struct ngx_buf_s  ngx_buf_t;
 
 struct ngx_buf_s {
+    /*
+    pos通常是用来告诉使用者本次应该从pos这个位置开始处理内存中的数，这样设置是因为同一个ngx_buf_t可能被多次反复处理。
+    当然，pos的含义是由使用它的模块定义的
+    */
     u_char          *pos;
+    /*
+    last通常表示有效的内容到此位置，注意，pos与last之间的内存是希望nginx处理的内容
+    */
     u_char          *last;
+    /*
+    处理文件时，file_pos与file_last的含义与处理内存时的pos与last相同，
+    file_pos表示将要处理的文件位置，file_last表示截至的文件位置
+    */
     off_t            file_pos;
     off_t            file_last;
-
+    /*
+    如果ngx_buf_t缓冲区用于内存，那么start指向这段内存的起始地址
+    */
     u_char          *start;         /* start of buffer */
     u_char          *end;           /* end of buffer */
-    ngx_buf_tag_t    tag;
-    ngx_file_t      *file;
+    ngx_buf_tag_t    tag;//表示当前缓冲区的类型，例如由哪个模块使用就指向这个模块ngx_module_t变量的地址
+    ngx_file_t      *file;//引用的文件
+    /*
+    当前缓冲区的影子缓冲区，该成员很少用到，仅仅在使用缓冲区转发上游服务器的响应时才使用了shadow成员，
+    这是因为nginx太节约内存了，分配一块内存并使用ngx_buf_t表示接收到的上游服务器响应后，在向下游客户端
+    转发时可能会把这块内存存储到文件中，也可能直接向下游发送，此时nginx绝不会重新复制一份内存用于新的目的，
+    而是再次建立一个ngx_buf_t结构体指向原内存，这样多个ngx_buf_t结构体指向了同一款内存，他们之间的关系就通过
+    shadow成员来引用。这种设计过于复杂，通常不建议使用
+    */
     ngx_buf_t       *shadow;
 
 
     /* the buf's content could be changed */
-    unsigned         temporary:1;
+    unsigned         temporary:1;//临时内存标志位，为1时表示数据在内存中且这段内存可以修改
 
     /*
      * the buf's content is in a memory cache or in a read only memory
      * and must not be changed
      */
-    unsigned         memory:1;
+    unsigned         memory:1;//标志位：为1时表示数据中内存中且这段内存不可用被修改
 
     /* the buf's content is mmap()ed and must not be changed */
-    unsigned         mmap:1;
+    unsigned         mmap:1;//标志位：为1时表示这段内存是用mmap系统调用映射过来的，不可用被修改
 
-    unsigned         recycled:1;
-    unsigned         in_file:1;
-    unsigned         flush:1;
+    unsigned         recycled:1;//为1时表示可回收
+    unsigned         in_file:1;//为1时表示这段缓冲区处理的实是文件而不是内存
+    unsigned         flush:1;//为1时表示需要执行flush操作
+    /*
+    对于操作这块缓冲区时是否使用同步方式，须谨慎考虑，这可能会阻塞nginx进程，nginx中所有操作几乎都是异步的，这是他支持
+    高并发的关键。有些框架代码在sync为1时可能会有阻塞的方式进行I/O操作，它的意义视使用它的nginx模块而定
+    */
     unsigned         sync:1;
+    /*
+    标志位：表示是否是最后一块缓冲区，因为ngx_buf_t可以由ngx_chain_t链表串联起来，
+    因此当last_buf为1时表示当前是最后一刻待处理的缓冲区
+    */
     unsigned         last_buf:1;
-    unsigned         last_in_chain:1;
+    unsigned         last_in_chain:1;//表示是否是ngx_chain_t中的最后一块缓冲区
 
-    unsigned         last_shadow:1;
-    unsigned         temp_file:1;
+    unsigned         last_shadow:1;//表示是否是最后一个影子缓冲区，与shadow域配合使用，通常不建议使用
+    unsigned         temp_file:1;//标志位：表示当前缓冲区是否属于临时文件
 
     /* STUB */ int   num;
 };
 
 
 struct ngx_chain_s {
-    ngx_buf_t    *buf;
-    ngx_chain_t  *next;
+    ngx_buf_t    *buf;//指向当前的ngx_buf_t缓冲区
+    ngx_chain_t  *next;//指向下一个ngx_chain_t
 };
 
 
--- src/core/ngx_list.h
+++ src/core/ngx_list.h
@@ -14,19 +14,30 @@
 
 
 typedef struct ngx_list_part_s  ngx_list_part_t;
-
+//描述链表中的一个元素
 struct ngx_list_part_s {
-    void             *elts;
-    ngx_uint_t        nelts;
+    void             *elts;//指向数组的起始地址
+    ngx_uint_t        nelts;//标识数组中已使用了多少个元素，当然nelts必须小于ngx_list_t结构体中的nalloc
     ngx_list_part_t  *next;
 };
 
-
+//描述整个链表
 typedef struct {
-    ngx_list_part_t  *last;
-    ngx_list_part_t   part;
+    ngx_list_part_t  *last;//指向链表的最后一个数组元素
+    ngx_list_part_t   part;//链表的首个数组元素
+    /*
+    链表中的每个ngx_list_part_t元素都是一个数组。因为数组存储的是某种类型的数据结构，
+    且ngx_list_t是非常灵活的数据结构，所以它不会限制存储什么样的数据，只是通过size
+    限制每个数组元素的占用空间大小，也就是用户要存储的一个数据所占用的字节数必须小于或等于size
+    */
     size_t            size;
+    /*
+    链表的数组元素一旦分配后是不可更改的。nalloc标识每个ngx_list_part_t数组的容量，即最多可存储多少个数据。
+    */
     ngx_uint_t        nalloc;
+    /*
+    链表中管理内存分配的内存池对象。用户要存放的数据占用的内存都是由pool分配的
+    */
     ngx_pool_t       *pool;
 } ngx_list_t;
 
