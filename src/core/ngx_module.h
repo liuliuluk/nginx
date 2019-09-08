1297f804411923db54724b3f92d64455f6dae69b
JessyLuk <lukxuan@foxmail.com>
2019-09-08 08:26:41 +0800

    update
    add comments

--- src/core/ngx_module.h
+++ src/core/ngx_module.h
@@ -220,32 +220,77 @@
 
 
 struct ngx_module_s {
+    /*
+    下面的ctx_index、index、spare0、spare1、spare2、spare3、
+    version变量不需要在定义时赋值，可以用Nginx准备好的宏NGX_MODULE_V1来
+    定义，它已经定义好了这7个值。
+    #define NGX_MODULE_V1
+    0，0，0，0，0，0，1
+    对于一类模块（由下面的type成员决定类别） 而言，ctx_index表示当前模
+    块在这类模块中的序号。这个成员常常是由管理这类模块的一个Nginx核心模块设
+    置的，对于所有的HTTP模块而言，ctx_index是由核心模块ngx_http_module
+    设置的。ctx_index非常重要，Nginx的模块化设计非常依赖于各个模块的顺
+    序，它们既用于表达优先级，也用于表明每个模块的位置，借以帮助Nginx框架快
+    速获得某个模块的数据
+    */
     ngx_uint_t            ctx_index;
     ngx_uint_t            index;
 
     char                 *name;
 
+    //spare系列的保留变量，暂未使用
     ngx_uint_t            spare0;
     ngx_uint_t            spare1;
 
-    ngx_uint_t            version;
+    ngx_uint_t            version;//模块的版本，便于将来的扩展，目前只有一种，默认为1
     const char           *signature;
 
+    /*
+    ctx用于指向一类模块的上下文结构体，为什么需要ctx呢？因为nginx模块有许多种类，不同类模块之间的功能
+    差别很大。例如，事件类型的模块主要处理I/O事件相关的功能，HTTP类型的模块主要处理HTTP应用层的功能。
+    这样每个模块都有了自己的特性，而ctx将会指向特定类型模块的公共接口，例如，在HTTP模块中，ctx需要指向ngx_http_module_t结构体
+    */
     void                 *ctx;
-    ngx_command_t        *commands;
+    ngx_command_t        *commands;//commands将处理nginx.conf中的配置项
+    /*
+    type表示该模块的类型，它与ctx指针是紧密相关的。在官方nginx中，它的取值范围是以下5中：NGX_HTTP_MODULE、NGX_CORE_MODULE、
+    NGX_CONF_MODULE、NGX_EVENT_MODULE、NGX_MAIL_MODULE。实际上还可以自定义新的模块类型
+    */
     ngx_uint_t            type;
+    /*
+    在nginx的启动、停止过程中，以下7个函数指针表示有7个执行点分别会调用这7种方法。对于任一个方法而言，如果不需要nginx在某个时刻
+    执行它，那么简单的把它设置为NULL指针即可。
 
+    */
+   /*
+   虽然从字面上理解应当在master进程启动时回调init_master，但到目前位置，框架代码从来不会调用它，因此可将init_master设为NULL
+   */
     ngx_int_t           (*init_master)(ngx_log_t *log);
-
+    /*
+    init_module回调方法在初始化所有模块时被调用，在master/worker模式下，这个阶段将在启动worker子进程前完成
+    */
     ngx_int_t           (*init_module)(ngx_cycle_t *cycle);
-
+    /*
+    init_process回调方法在正常服务前被调用。在master/worker模式下，多个worker子进程已经产生，在每个worker进程的初始化
+    过程会调用所有模块的init_process函数
+    */
     ngx_int_t           (*init_process)(ngx_cycle_t *cycle);
+    /*
+    由于nginx暂不支持多线程模式，所以init_thread在框架代码中没有被调用过，设为NULL
+    */
     ngx_int_t           (*init_thread)(ngx_cycle_t *cycle);
+    //同上，exit_thread也不支持，设为NULL
     void                (*exit_thread)(ngx_cycle_t *cycle);
+    /*
+    exit_process回调方法在服务停止前调用，在master/worker模式下，worker进程会在退出前调用它
+    */
     void                (*exit_process)(ngx_cycle_t *cycle);
-
+    //exit_master回调方法将在master进程退出前被调用
     void                (*exit_master)(ngx_cycle_t *cycle);
-
+    /*
+    以下8个spare_hook变量也是保留字段，目前没有使用，但可用nginx提供的NGX_MODULE_V1_PADDING宏来填充。看一下该宏的定义：
+    #define NGX_MODULE_V1_PADDING 0,0,0,0,0,0,0,0
+    */
     uintptr_t             spare_hook0;
     uintptr_t             spare_hook1;
     uintptr_t             spare_hook2;
